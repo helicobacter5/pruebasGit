ac1d794f795bde4292d881ea3583a02254231cec
U0193562 <u0193562@lacaixa.es>
2018-11-28 10:05:07 +0100

    segundo cambio

--- src/com/helicobacter5/prueba/Prueba.java
+++ src/com/helicobacter5/prueba/Prueba.java
@@ -4,11 +4,11 @@ package com.helicobacter5.prueba;
 
 public class Prueba {
 public static void main(String[] args) {
-    String [] bebidasCarlos = {"kas limon", "cocacola"};
+    String [] bebidas2 = {"kas limon", "cocacola"};
     
-    for (int i = 0; i < bebidasCarlos.length; i++) {
-        String string = bebidasCarlos[i];
-        System.out.println("Hay una bebida llamada: "+bebidasCarlos[i]);
+    for (int i = 0; i < bebidas2.length; i++) {
+        String string = bebidas2[i];
+        System.out.println("Hay una bebida llamada: "+bebidas2[i]);
         
     }
 }
