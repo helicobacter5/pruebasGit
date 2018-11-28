6296faa6d01114924473d94231edaaffcb54f4bd
0015581 <bfeijoo@viewnext.com>
2018-11-28 10:57:41 +0100

    Commit Beni

--- src/com/helicobacter5/prueba/Prueba.java
+++ src/com/helicobacter5/prueba/Prueba.java
@@ -4,11 +4,11 @@ package com.helicobacter5.prueba;
 
 public class Prueba {
 public static void main(String[] args) {
-    String [] bebidas = {"kas limon", "cocacola"};
+    String [] refrescos = {"kas limon", "cocacola"};
     
-    for (int i = 0; i < bebidas.length; i++) {
-        String string = bebidas[i];
-        System.out.println("Hay una bebida llamada: "+bebidas[i]);
+    for (int i = 0; i < refrescos.length; i++) {
+        String string = refrescos[i];
+        System.out.println("Hay una bebida llamada: "+refrescos[i]);
         
     }
 }
