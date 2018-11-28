5a8177563c94cb66589f745b827d12633b1909df
U0193562 <u0193562@lacaixa.es>
2018-11-28 09:10:27 +0100

    un commit inicial

---
+++ src/com/helicobacter5/prueba/Prueba.java
@@ -0,0 +1,15 @@
+package com.helicobacter5.prueba;
+
+
+
+public class Prueba {
+public static void main(String[] args) {
+    String [] bebidas = {"kas limon", "cocacola"};
+    
+    for (int i = 0; i < bebidas.length; i++) {
+        String string = bebidas[i];
+        System.out.println("Hay una bebida llamada: "+bebidas[i]);
+        
+    }
+}
+}
