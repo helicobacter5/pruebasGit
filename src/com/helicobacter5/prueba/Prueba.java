7acee335b9170a7adcada156d8192697989a9623
U0193562 <u0193562@lacaixa.es>
2018-11-28 09:22:19 +0100

    Cambiado el nombre de lavariable

--- src/com/helicobacter5/prueba/Prueba.java
+++ src/com/helicobacter5/prueba/Prueba.java
@@ -4,11 +4,11 @@ package com.helicobacter5.prueba;
 
 public class Prueba {
 public static void main(String[] args) {
-    String [] bebidas = {"kas limon", "cocacola"};
+    String [] bebidasCarlos = {"kas limon", "cocacola"};
     
-    for (int i = 0; i < bebidas.length; i++) {
-        String string = bebidas[i];
-        System.out.println("Hay una bebida llamada: "+bebidas[i]);
+    for (int i = 0; i < bebidasCarlos.length; i++) {
+        String string = bebidasCarlos[i];
+        System.out.println("Hay una bebida llamada: "+bebidasCarlos[i]);
         
     }
 }
